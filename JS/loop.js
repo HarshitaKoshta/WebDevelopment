b400c242f3232e03469ff0ef0f77d91c70c723e0
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-02 22:58:55 +0530

    while loop

--- JS/loop.js
+++ JS/loop.js
@@ -3,9 +3,15 @@
 // }
 // console.log("end")
 
-let sum =0;
-let n=7;
-for(let i=1;i<=n;i++){
-    sum +=i;
+// let sum =0;
+// let n=7;
+// for(let i=1;i<=n;i++){
+//     sum +=i;
+// }
+// console.log(sum);
+
+let i=1;
+while(i<=10){
+    console.log("abc");
+    i++;
 }
-console.log(sum);
