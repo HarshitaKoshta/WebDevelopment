bb2273b8a496f93f01646986f3b22466302d8de7
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-02 22:47:07 +0530

    sum of n

--- JS/loop.js
+++ JS/loop.js
@@ -1,4 +1,11 @@
-for(let count =1;count<=5;count++){
-    console.log("Runnn")
+// for(let count =1;count<=5;count++){
+//     console.log("Runnn")
+// }
+// console.log("end")
+
+let sum =0;
+let n=7;
+for(let i=1;i<=n;i++){
+    sum +=i;
 }
-console.log("end")
+console.log(sum);
