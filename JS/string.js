df2dadf25f199573c662891b2ffe9df267f89600
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-03 18:47:56 +0530

    template literal ..string

---
+++ JS/string.js
@@ -0,0 +1,9 @@
+// let str = "harshita";
+// console.log(str.length);
+// console.log(str[6]);
+
+let obj={
+    item : "pen",
+    price : 15
+}
+console.log(`The cost of ${obj.item} is ${obj.price} rupees`); ////Template literals
