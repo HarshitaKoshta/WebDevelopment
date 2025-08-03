78c22d7dce2a2e83ffd2f2405c3e4fb7257ee9f6
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-03 19:07:20 +0530

    string

--- JS/string.js
+++ JS/string.js
@@ -1,9 +1,25 @@
-// let str = "harshita";
+// let str = "Harshita";
+// let str1 = " koshta";
 // console.log(str.length);
-// console.log(str[6]);
+// // console.log(str[6]);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.trim());///remove white space
+// console.log(str.slice(2,7));
+// console.log(str.concat(str1));
+// console.log(str.replace("H","h"));
 
-let obj={
-    item : "pen",
-    price : 15
-}
-console.log(`The cost of ${obj.item} is ${obj.price} rupees`); ////Template literals
+
+
+// let obj={
+//     item : "pen",
+//     price : 15
+// }
+// console.log(`The cost of ${obj.item} is ${obj.price} rupees`); ////Template literals
+
+
+//////game 
+
+let name = prompt("Enter your full name");
+let username = "@"+ name + name.length;
+console.log(username)
