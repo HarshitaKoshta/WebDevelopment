82a1d7a949a29d679f97dc0c89bc4828a6455744
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-04 18:31:20 +0530

    count vowel

--- JS/oop.js
+++ JS/oop.js
@@ -1,26 +1,49 @@
-///********function*********
+// ///********function*********
 
-function add(a,b){
-    console.log(a+b);
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(2,6);
+
+// function mul(a,b){
+//     return a*b;
+// }
+// let c = mul(3,4);
+// console.log(c);
+
+// ///************Arrow function************
+// const arrowsum = (a,b)=>{
+//     console.log(a+b);
+// };
+
+// let multi = (a,b)=>{
+//     console.log(a*b);
+// };
+// multi(5,6);
+
+
+/* practice question count vowel in string */
+
+function strCount(str){
+    let count = 0;
+    for(const char of str){
+        if(char == 'a' || char == 'e' ||char == 'i' ||char == 'o' ||char == 'u'){
+            count++;
+        }
+    }
+    console.log(count);
 }
-add(2,6);
+strCount("harshitaa");
 
-function mul(a,b){
-    return a*b;
+
+const countVow = (str) =>{
+     let count = 0;
+    for(const char of str){
+        if(char == 'a' || char == 'e' ||char == 'i' ||char == 'o' ||char == 'u'){
+            count++;
+        }
+    }
+    console.log(count);
 }
-let c = mul(3,4);
-console.log(c);
-
-///************Arrow function************
-const arrowsum = (a,b)=>{
-    console.log(a+b);
-};
-
-let multi = (a,b)=>{
-    console.log(a*b);
-};
-multi(5,6);
-
-
-/* practice question */
+countVow("harshita");
 
