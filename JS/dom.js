c54c11613c0d0712d69162411607eb08d17c02aa
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-06 18:48:18 +0530

    get element by class or id

---
+++ JS/dom.js
@@ -0,0 +1,7 @@
+let heading = document.getElementById("heading1");
+console.dir(heading);
+console.log(heading)
+
+let headings = document.getElementsByClassName("heading2");
+console.dir(headings);
+console.log(headings);
