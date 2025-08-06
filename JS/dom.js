b3560b16aa803a748292fdc9aa08228b50d70d85
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-06 18:55:46 +0530

    query selector

--- JS/dom.js
+++ JS/dom.js
@@ -1,7 +1,16 @@
-let heading = document.getElementById("heading1");
-console.dir(heading);
-console.log(heading)
+// let heading = document.getElementById("heading1");
+// console.dir(heading);
+// console.log(heading)
 
-let headings = document.getElementsByClassName("heading2");
-console.dir(headings);
-console.log(headings);
+// let headings = document.getElementsByClassName("heading2");
+// console.dir(headings);
+// console.log(headings);
+
+let parahs = document.getElementsByTagName("p");
+console.dir(parahs);
+
+let firstEle = document.querySelector("p");
+console.dir(firstEle);
+
+let allEle = document.querySelectorAll("p");
+console.dir(allEle);
