187179741e2ac9b1e7fd646258720eb4193e65ad
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-07 19:18:41 +0530

    add text to divs

--- JS/dom.js
+++ JS/dom.js
@@ -15,7 +15,17 @@
 // let allEle = document.querySelectorAll("p");
 // console.dir(allEle);
 
-let h2 = document.querySelector("h2");
-console.dir(h2.innerText);
-h2.innerText = h2.innerText + "from Harshita";
-console.log(h2.innerText)
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+// h2.innerText = h2.innerText + "from Harshita";
+// console.log(h2.innerText)
+
+let divs = document.querySelectorAll(".box");
+console.log(divs[0]);
+
+let idx = 1;
+for(div of divs){
+    // console.log(div.innerText);
+    div.innerText = `new unique value ${idx}`;
+    idx++;
+}
