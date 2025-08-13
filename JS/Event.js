d9c26be340f1a2c17121702d043040cc989f5828
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-13 20:59:29 +0530

    dark-light mode

--- JS/Event.js
+++ JS/Event.js
@@ -1,4 +1,4 @@
-let btn1 = document.querySelector("#btn1");
+// let btn1 = document.querySelector("#btn1");
 
 // btn1.onclick = (e) => {
 //     console.log(e);
@@ -16,16 +16,34 @@ let btn1 = document.querySelector("#btn1");
 //    console.log(e.clientX,e.clientY);
 // };
 
-btn1.addEventListener("click",(evt) =>{
-   console.log("button1 was clicked");
-   console.log(evt);
-   console.log(evt.type);
-});
+// btn1.addEventListener("click",(evt) =>{
+//    console.log("button1 was clicked");
+//    console.log(evt);
+//    console.log(evt.type);
+// });
 
-btn1.addEventListener("click",() =>{
-   console.log("button1 was clicked -- handler2");
-});
+// btn1.addEventListener("click",() =>{
+//    console.log("button1 was clicked -- handler2");
+// });
 
-btn1.removeEventListener("click",() =>{
-   console.log("button1 was clicked -- handler2");
+// btn1.removeEventListener("click",() =>{
+//    console.log("button1 was clicked -- handler2");
+// });
+
+
+////////////question////////
+
+let modeBtn = document.querySelector("#mode");
+let currMode = "light";
+
+modeBtn.addEventListener("click",()=>{
+   if(currMode==="light"){
+      currMode = "dark";
+      document.querySelector("body").style.backgroundColor = "black";
+   }else{
+      currMode = "light";
+      document.querySelector("body").style.backgroundColor = "pink";
+
+   }
+   console.log(currMode);
 });
