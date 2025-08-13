4c9486ce1bbae3c28ad2e5b815b97f04cd83fe9e
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-08-13 20:45:41 +0530

    event listener

--- JS/Event.js
+++ JS/Event.js
@@ -1,19 +1,31 @@
 let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = (e) => {
-    console.log(e);
-   console.log(e.type);
-   console.log(e.target);
-   console.log(e.clientX,e.clientY);
-};
+// btn1.onclick = (e) => {
+//     console.log(e);
+//    console.log(e.type);
+//    console.log(e.target);
+//    console.log(e.clientX,e.clientY);
+// };
 
-let div = document.querySelector("div");
+// let div = document.querySelector("div");
 
-div.onmouseover = (e) => {
-   console.log(e);
-   console.log(e.type);
-   console.log(e.target);
-   console.log(e.clientX,e.clientY);
+// div.onmouseover = (e) => {
+//    console.log(e);
+//    console.log(e.type);
+//    console.log(e.target);
+//    console.log(e.clientX,e.clientY);
+// };
 
-};
+btn1.addEventListener("click",(evt) =>{
+   console.log("button1 was clicked");
+   console.log(evt);
+   console.log(evt.type);
+});
 
+btn1.addEventListener("click",() =>{
+   console.log("button1 was clicked -- handler2");
+});
+
+btn1.removeEventListener("click",() =>{
+   console.log("button1 was clicked -- handler2");
+});
