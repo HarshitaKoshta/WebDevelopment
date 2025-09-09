062f62624937725086308ce09dd80fa22090d41e
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-09-09 20:09:16 +0530

    dom

---
+++ lecture15/script.js
@@ -0,0 +1,28 @@
+// let h1 = document.getElementById("one")
+// console.log(h1)
+// h1.style.color = "purple"
+// for(let i of ) 
+
+// let h = document.getElementsByClassName("helo")
+// console.log(h)
+// for(let i of h){
+//     i.style.color = "rgba(3,9,99)"
+// } 
+
+// let h = document.querySelector("#one")
+// h.innerText = "hiiiiiii"
+// console.log(h.innerText);
+
+// let h = document.querySelector(".helo")
+// console.log(h)        --->> it select only 1st elemnt
+
+
+// let a = document.querySelector("a")
+// a.setAttribute("href","http://flipkart.com")
+// console.log()
+
+let body = document.querySelector("body")
+
+function func1(){
+    body.style.backgroundColor = "skyblue";
+}
