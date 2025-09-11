e8579363fb176a1d506da6f1ae763e31c712a3b2
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-09-11 23:29:27 +0530

    local storage

--- lecture16/form.js
+++ lecture16/form.js
@@ -1,28 +1,43 @@
-let formm  = document.querySelector("form")
+let form  = document.querySelector("#formm")
 
-formm.addEventListener("submit",(e)=>{
+form.addEventListener("submit",(e)=>{
       e.preventDefault()
-    //   for(let i in formm){
-    //     console.log(formm[i].value);
-    //   }
     //   console.log(formm[0].value);
     //   console.log(formm[1].value);
     //   console.log(formm[2].value);
     //   console.log(formm[3].value); 
 
     let data = {
-        name : formm[1].value,
-        email : formm[2].value,
-        pass : formm[3].value
+        name : form[1].value,
+        email : form[2].value,
+        pass : form[3].value
     }
     console.log(data);
     localStorage.setItem("formData",JSON.stringify(data))
     
-    formm.addEventListener("reset",(e)=>{
+    form.addEventListener("reset",(e)=>{
     localStorage.removeItem("formData",JSON.stringify(data))
 })
-
 })
+
+let form2 = document.querySelector("#form2")
+form2.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let saveData = localStorage.getItem("formData")
+    let dataObj = JSON.parse(saveData)
+
+    let logIn = {
+    email:form[1].value,
+    pass:form[2].value
+    }
+
+    if(data.email == logIn.email &&data.pass == logIn.pass){
+        prompt("login successfull")
+    }else{
+        prompt("error ")
+    }
+})
+
 
 
 
