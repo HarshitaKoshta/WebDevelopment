453081242341700daba6c090ec1c7d31a468365b
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-09-23 22:58:07 +0530

    time

--- ReactJs/my-react-app/src/Time.jsx
+++ ReactJs/my-react-app/src/Time.jsx
@@ -4,15 +4,25 @@ const Time = () => {
     
     let [count,SetCount] = useState()
     setInterval(()=>{
-        SetCount(new Date().toLocalTimeString())
+        SetCount(new Date().toLocaleTimeString())
     })
     // console.log(data)
   return (
     <div>
         <h2>{count}</h2>
-       
+        <Color/>
     </div>
   )
 }
+
+// import React from 'react'
+
+  function Color(){
+    return(
+      <div>
+         colour
+      </div>
+    )
+  }
 
 export default Time
