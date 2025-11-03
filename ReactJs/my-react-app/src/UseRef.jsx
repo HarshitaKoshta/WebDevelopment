27efcfae04ec8eb4521a148a6cdc56aa83813540
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-11-03 18:11:28 +0530

    useref

--- ReactJs/my-react-app/src/UseRef.jsx
+++ ReactJs/my-react-app/src/UseRef.jsx
@@ -1,19 +1,39 @@
-import React, { useState,useRef, useEffect } from 'react'
+// import React, { useState,useRef, useEffect } from 'react'
+
+// const UseRef = () => {
+//     let [count,setCount] = useState(0)
+//     const a = useRef(0);
+
+//     useEffect(()=>{
+//         a.current ++;
+//         console.log(`rerendering and the value of a is ${a.current}`);
+        
+//     },[])
+//   return (
+//     <div>
+//         <h2>Count: {count}</h2>
+//         <button onClick={() => setCount(count + 1)}>Increase</button>
+//         <p>Component re-rendered: {a.current} times</p>
+//     </div>
+//   )
+// }
+
+// export default UseRef
+
+import React, { useEffect, useState, useRef } from 'react'
 
 const UseRef = () => {
-    let [count,setCount] = useState(0)
-    const a = useRef(0);
+  const [input, setinput] = useState("")
+  
+  let count = useRef(0)
 
-    useEffect(()=>{
-        a.current ++;
-        console.log(`rerendering and the value of a is ${a.current}`);
-        
-    },[])
+  useEffect(()=>{
+    count.current += 1
+  })
   return (
     <div>
-        <h2>Count: {count}</h2>
-        <button onClick={() => setCount(count + 1)}>Increase</button>
-        <p>Component re-rendered: {a.current} times</p>
+      <h1>{count.current}</h1>
+      <input type="text" onChange={(e)=>setinput(e.target.value)} />
     </div>
   )
 }
